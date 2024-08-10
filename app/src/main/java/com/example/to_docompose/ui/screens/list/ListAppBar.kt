5f2fc7a0515501f3ed0c1a9f552d73b4d6474e8d
TienLV <l.vantien286@gmail.com>
2024-08-10 17:18:19 +0700

    Add List AppBar Action

---
+++ app/src/main/java/com/example/to_docompose/ui/screens/list/ListAppBar.kt
@@ -0,0 +1,180 @@
+package com.example.to_docompose.ui.screens.list
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.to_docompose.R
+import com.example.to_docompose.components.PriorityItem
+import com.example.to_docompose.data.models.Priority
+import com.example.to_docompose.ui.theme.LARGE_PADDING
+import com.example.to_docompose.ui.theme.Typography
+import com.example.to_docompose.ui.theme.topAppBarBackgroundColor
+import com.example.to_docompose.ui.theme.topAppBarContentColor
+
+@Composable
+fun ListAppBar() {
+    DefaultListAppBar(
+        onSearchClicked = {},
+        onSortClicked = {},
+        onDeleteClicked = {},
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DefaultListAppBar(
+    onSearchClicked: () -> Unit,
+    onSortClicked: (priority: Priority) -> Unit,
+    onDeleteClicked: () -> Unit,
+) {
+    TopAppBar(
+        title = {
+            Text(text = stringResource(R.string.tasks), color = MaterialTheme.colorScheme.topAppBarContentColor)
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.topAppBarBackgroundColor
+        ),
+        actions = {
+            ListAppBarActions(
+                onSearchClicked = onSearchClicked,
+                onSortClicked = onSortClicked,
+                onDeleteClicked = onDeleteClicked,
+            )
+        }
+    )
+}
+
+@Composable
+fun ListAppBarActions(
+    onSearchClicked: () -> Unit,
+    onSortClicked: (priority: Priority) -> Unit,
+    onDeleteClicked: () -> Unit,
+) {
+    SearchAction(onSearchClicked = onSearchClicked)
+    SortAction(onSortClicked = onSortClicked)
+    DeleteAllAction(onDeleteClicked = onDeleteClicked)
+}
+
+@Composable
+fun SearchAction(onSearchClicked: () -> Unit) {
+    IconButton(
+        onClick = onSearchClicked
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Search,
+            contentDescription = stringResource(R.string.search_tasks),
+            tint = MaterialTheme.colorScheme.topAppBarContentColor
+        )
+
+    }
+}
+
+@Composable
+fun SortAction(
+    onSortClicked: (priority: Priority) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(
+        onClick = { expanded = true },
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_filter_list),
+            contentDescription = stringResource(R.string.sort_tasks),
+            tint = MaterialTheme.colorScheme.topAppBarContentColor
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = { PriorityItem(priority = Priority.LOW) },
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.LOW)
+                })
+            DropdownMenuItem(
+                text = { PriorityItem(priority = Priority.MEDIUM) },
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.MEDIUM)
+                })
+            DropdownMenuItem(
+                text = { PriorityItem(priority = Priority.HIGH) },
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.HIGH)
+                })
+            DropdownMenuItem(
+                text = { PriorityItem(priority = Priority.NONE) },
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.NONE)
+                })
+        }
+    }
+}
+
+@Composable
+fun DeleteAllAction(
+    onDeleteClicked: () -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(
+        onClick = { expanded = true },
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_vertical_menu),
+            contentDescription = stringResource(R.string.delete_all),
+            tint = MaterialTheme.colorScheme.topAppBarContentColor
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = {
+                    Text(
+                        modifier = Modifier.padding(start = LARGE_PADDING),
+                        text = stringResource(R.string.delete_all),
+                        style = Typography.titleMedium
+                    )
+                },
+                onClick = {
+                    expanded = false
+                    onDeleteClicked()
+                })
+
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun DefaultListAppBarPreview() {
+    DefaultListAppBar(
+        onSearchClicked = {},
+        onSortClicked = {},
+        onDeleteClicked = {},
+    )
+}
--- app/src/main/java/com/example/to_docompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/example/to_docompose/ui/screens/list/ListScreen.kt
@@ -1,6 +1,7 @@
 package com.example.to_docompose.ui.screens.list
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -8,6 +9,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,18 +19,26 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.to_docompose.R
+import com.example.to_docompose.ui.theme.fabBackgroundColor
 
 @Composable
 fun ListScreen(navigateToTaskScreen: (taskId: Int) -> Unit) {
     Scaffold(
+        topBar = {
+            ListAppBar()
+        },
         content = { paddingValues ->
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(paddingValues),
+                    .padding(paddingValues)
+                    .background(color = MaterialTheme.colorScheme.background),
                 contentAlignment = Alignment.Center
             ) {
-                Text(text = "Hello, Scaffold!")
+                Text(
+                    text = "Hello, Scaffold!",
+                    color = MaterialTheme.colorScheme.onSurface
+                )
             }
         },
         floatingActionButton = { ListFab(navigateToTaskScreen) }
@@ -37,7 +47,11 @@ fun ListScreen(navigateToTaskScreen: (taskId: Int) -> Unit) {
 
 @Composable
 fun ListFab(onFabClicked: (taskId: Int) -> Unit) {
-    FloatingActionButton(onClick = { onFabClicked(-1) }) {
+    FloatingActionButton(
+        onClick = { onFabClicked(-1) },
+        containerColor = MaterialTheme.colorScheme.fabBackgroundColor
+
+    ) {
         Icon(
             imageVector = Icons.Filled.Add,
             contentDescription = stringResource(id = R.string.add_button),
