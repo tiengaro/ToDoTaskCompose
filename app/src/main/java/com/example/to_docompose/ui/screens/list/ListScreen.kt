e98fbc5f6edd444d0eb53a7e5fae2657ded6cfed
TienLV <l.vantien286@gmai.com>
2024-08-12 23:42:11 +0700

    add fab button

--- app/src/main/java/com/example/to_docompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/example/to_docompose/ui/screens/list/ListScreen.kt
@@ -3,10 +3,13 @@ package com.example.to_docompose.ui.screens.list
 import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -19,16 +22,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
@@ -40,84 +36,49 @@ import androidx.compose.ui.unit.dp
 import com.example.to_docompose.R
 import com.example.to_docompose.ui.theme.fabBackgroundColor
 import kotlinx.collections.immutable.PersistentList
-import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlin.random.Random
 
 data class Test(
-    var id: MutableState<Int>,
+    val id: Int,
     val test: String
 )
 
-data class ComplexModel(
-    val id: Int,
-    val name: MutableState<String>,
-    val description: MutableState<String>,
-    var items: PersistentList<String>
+fun getRandomColor() = Color(
+    Random.nextInt(256),
+    Random.nextInt(256),
+    Random.nextInt(256),
+    alpha = 255
 )
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
-fun ComplexModelExample() {
-    val testArray = listOf(
-        Test(1, "1"),
-        Test(1, "2"),
-        Test(1, "3"),
-        Test(1, "4"),
-    )
-    val test = MutableSharedFlow<List<Test>>(replay = 1)
-    val state by test.collectAsState(arrayListOf())
+fun ComplexModelExample(test: SnapshotStateList<Test>, onClick: (item: Test) -> Unit) {
 
-    LaunchedEffect(key1 = "a") {
-        test.emit(testArray)
-    }
 
-    val model = remember {
-        ComplexModel(
-            id = 1,
-            name = mutableStateOf("Item 1"),
-            description = mutableStateOf("Description 1"),
-            items = persistentListOf("Subitem 1", "Subitem 2", "Subitem 3")
-        )
-    }
-
-    // Derived state to combine name and description
-    val combinedText by remember {
-        derivedStateOf { "${model.name.value}: ${model.description.value}" }
-    }
 
     Column(modifier = Modifier.fillMaxSize()) {
-        TextField(
-            value = model.name.value,
-            onValueChange = { model.name.value = it },
-            label = { Text("Name") }
-        )
-
-        TextField(
-            value = model.description.value,
-            onValueChange = { model.description.value = it },
-            label = { Text("Description") }
-        )
-
-        Text(text = combinedText)
-
-        LazyColumn(modifier = Modifier.weight(1f)) {
-            items(model.items, key = { it }) { item ->
-                Log.d("aaaaaaaaa", item)
-                BasicText(
-                    text = item,
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(test, key = { it.hashCode() }) { item ->
+                Text(
+                    text = item.test,
                     modifier = Modifier
                         .fillMaxWidth()
+                        .height(56.dp)
                         .padding(16.dp)
-                )
+                        .border(3.dp, getRandomColor())
+                        .align(Alignment.CenterHorizontally)
+                        .clickable {
+                            onClick.invoke(item)
+                        }
+                    )
             }
         }
 
-        Button(onClick = {
-            // Update items in SnapshotStateList
-            model.items = model.items.add("Subitem ${model.items.size + 1}")
-        }) {
-            Text("Add Item")
-        }
+
+//        Button(onClick = onClick) {
+//            Text("Add Item")
+//        }
     }
 }
 
@@ -128,9 +89,26 @@ fun ListScreen(navigateToTaskScreen: (taskId: Int) -> Unit) {
 
 
     val onSearchClicked = {
-        testArray[0].id.value++
-        val a = test.tryEmit(testArray)
-        val b = Log.d("aaaaaaaaa", a.toString())
+
+    }
+    val testArray = arrayListOf<Test>().apply {
+        for (i in 0..15) {
+            this.add(Test(i, "$i"))
+        }
+
+    }
+    val test: SnapshotStateList<Test> = mutableStateListOf<Test>().apply {
+        addAll(testArray)
+    }
+
+    var selected = 0
+    val onClick = remember {
+        {item: Test ->
+            test[selected] = test[selected].copy(test = "${selected}")
+            test[item.id] = test[item.id].copy(test = "${item.id} Selected")
+
+            selected  = item.id
+        }
     }
 
     Scaffold(
@@ -147,7 +125,9 @@ fun ListScreen(navigateToTaskScreen: (taskId: Int) -> Unit) {
                     .background(color = MaterialTheme.colorScheme.background),
                 contentAlignment = Alignment.Center
             ) {
-                ComplexModelExample()
+
+
+                ComplexModelExample(test, onClick)
             }
         },
         floatingActionButton = { ListFab(navigateToTaskScreen) }
